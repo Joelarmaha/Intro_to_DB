c4e6eb71b5c340c4ed3c24a5d24ea58574c69cf5
Joel Amah Ayesou <joelarmaha@gmail.com>
2025-06-28 14:52:24 +0000

    alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,36 +1,36 @@
 CREATE DATABASE IF NOT EXISTS  alx_book_store;
-use alx_book_store;
+USE alx_book_store;
 
-create table books(
-	book_id INT primary key not null,
+CREATE TABLE books(
+	book_id INT PRIMARY KEY,
 	title VARCHAR(130),
-	author_id int,
-    foreign key(author_id) references Authors(author_id),
+	author_id INT,
+    FOREIGN KEY(author_id) REFERENCES Authors(author_id),
 	price DOUBLE,
 	publication_date DATE); 
 
-create table Authors(
-	author_id int primary key,
+CREATE TABLE Authors(
+	author_id INT PRIMARY KEY,
     author_name VARCHAR(215)
     );    
-create table Customers(
-	customer_id int primary key,
+CREATE TABLE Customers(
+	customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
     );
     
-create table Orders(
-	order_id int Primary Key,
-    customer_id int,
-    foreign key(customer_id) references Customers(customer_id));
+CREATE TABLE Orders(
+	order_id INT PRIMARY KEY,
+    customer_id INT,
+    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id));
     
-create table Order_Details(
-	orderdetailid int primary key,
-    order_id int,
-    foreign key(order_id) references Orders(order_id),
-    book_id int,
-    foreign key(book_id) references books(book_id),
+CREATE TABLE Order_Details(
+	orderdetailid INT PRIMARY KEY,
+    order_id INT,
+    FOREIGN KEY(order_id) REFERENCES Orders(order_id),
+    book_id INT,
+    FOREIGN KEY(book_id) REFERENCES books(book_id),
     quantity DOUBLE);
     
 
